7fb7c691a49cf3b535caede76e95def38a2d3080
PatriceG <patrice.godard@laposte.net>
2024-04-10 15:14:01 +0200

    implemended optional basic authentication (#42)
    Co-authored-by: GODARD Patrice <patrice.godard@orange.com>

--- prometheus/utils.go
+++ prometheus/utils.go
@@ -2,6 +2,7 @@ package prometheus
 
 import (
 	"context"
+	"net/http"
 	"os"
 
 	"github.com/prometheus/client_golang/api"
@@ -11,6 +12,18 @@ import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 )
 
+// roundTripperWithBasicAuth is a custom RoundTripper that includes basic authentication headers
+type roundTripperWithBasicAuth struct {
+	http.RoundTripper
+	username string
+	password string
+}
+
+func (rt *roundTripperWithBasicAuth) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.SetBasicAuth(rt.username, rt.password)
+	return rt.RoundTripper.RoundTrip(req)
+}
+
 func connect(ctx context.Context, d *plugin.QueryData) (v1.API, error) {
 	return connectRaw(ctx, d.ConnectionCache, d.Connection)
 }
@@ -18,6 +31,8 @@ func connect(ctx context.Context, d *plugin.QueryData) (v1.API, error) {
 func connectRaw(ctx context.Context, cc *connection.ConnectionCache, c *plugin.Connection) (v1.API, error) {
 
 	var address string
+	username := ""
+	password := ""
 
 	// Load connection from cache, which preserves throttling protection etc
 	cacheKey := "prometheus"
@@ -33,14 +48,30 @@ func connectRaw(ctx context.Context, cc *connection.ConnectionCache, c *plugin.C
 		address = *prometheusConfig.Address
 	}
 
+	if prometheusConfig.Username != nil {
+		username = *prometheusConfig.Username
+	}
+	if prometheusConfig.Password != nil {
+		password = *prometheusConfig.Password
+	}
+
 	// Error if the minimum config is not set
 	if address == "" {
 		// Panic since we cannot create a valid empty API to return
 		panic("address must be configured")
 	}
 
+	//handle authentication
+	rt := api.DefaultRoundTripper
+	rt = &roundTripperWithBasicAuth{
+		RoundTripper: rt,
+		username:     username,
+		password:     password,
+	}
+
 	client, err := api.NewClient(api.Config{
-		Address: address,
+		Address:      address,
+		RoundTripper: rt,
 	})
 
 	if err != nil {
